e908cc4d8fe3e22e1ed234a1c6257be4672010fa
ThanhTrieuVo <ttrieu@lunageo.com>
2026-06-21 23:03:06 -0400

    Handle encoded database URLs in Alembic

--- apps/api/app/data/migrations/env.py
+++ apps/api/app/data/migrations/env.py
@@ -24,8 +24,10 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-config.set_main_option("sqlalchemy.url", settings.database_url)
-
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url.replace("%", "%%"),
+)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
